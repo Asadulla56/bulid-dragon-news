d6257d6d21f8964a0de891da9dc1d31b98967680
Asadulla56 <mdasadullahoque294@gmail.com>
2024-10-20 01:17:49 +0600

    set up commite

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbcmBN4Osg0s-O5tQCBcsGmee3dyiCUJ4",
+  authDomain: "bulid-dragon-news.firebaseapp.com",
+  projectId: "bulid-dragon-news",
+  storageBucket: "bulid-dragon-news.appspot.com",
+  messagingSenderId: "471185818872",
+  appId: "1:471185818872:web:d5e23d7a42f54ce7d0a70d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
